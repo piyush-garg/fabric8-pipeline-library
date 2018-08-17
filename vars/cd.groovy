62e9b5cbb8f624a43f9c30797cc09902ce62056f
hriships <hrishishinde.p@gmail.com>
2018-08-17 12:28:17 +0530

    test cd closure

--- vars/cd.groovy
+++ vars/cd.groovy
@@ -2,14 +2,15 @@
 
 def call(Closure body) {
     //def resources = processTemplate(templateConfig)
-    //def config = [templateConfig: "cd"]
+    //def config = [templateConfig1: "cd"]
 
     if(env.BRANCH_NAME.equals('master')) {
-
+        echo "templateconfig ${body.templateConfig}"
+        echo "resoures ${resources}"
+        //echo "resources ${body.resources}"
         //body.resolveStrategy = Closure.DELEGATE_FIRST
         //body.delegate = config
         body()
-
         //echo "cd config ${config}"
     }
 }
--- vars/nodeJs.groovy
+++ vars/nodeJs.groovy
@@ -4,16 +4,19 @@ def call(Map parameters = [:], Closure body) {
 
     def defaultLabel = buildId('nodejs')
     def label = parameters.get('label', defaultLabel)
-    def config = [templateConfig: "nodeJs"]
-
+    def resources = "hello"
+    //def config = [templateConfig: "nodeJs"]
+    //def config = ["template":"processed templateg"]
     node {
         //checkout scm
 
-        body.resolveStrategy = Closure.DELEGATE_FIRST
-        body.delegate = config
+        //body.resolveStrategy = Closure.DELEGATE_FIRST
+        //body.delegate = config
+        echo "templateconfig ${body.templateConfig}"
+        body.cd.setProperty("resources" , "resources")
         body()
 
-        echo "nodeJs config ${config}"
+        //echo "nodeJs config ${config}"
     }
 
 }
