64ccf26723b06e8359965f445d7dbe1adfb3e944
hriships <hrishishinde.p@gmail.com>
2018-08-20 18:56:38 +0530

    fixed function

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,17 +1,17 @@
 #!/usr/bin/groovy
 import io.fabric8.Utils;
 
-def call(Map resources, String envName, Map args = [:]) {
+def call(Map args = [:]) {
     def userNamespace = new Utils().getUsersNamespace();
-    def deployNamespace = userNamespace + "-" + envName;
+    def deployNamespace = userNamespace + "-" + args.env;
 
     if (args.approval == 'manual') {
         askForInput()
     }
 
-    stage ("Deploy to ${envName}") {
-        tagImageToDeployEnv(deployNamespace, userNamespace, resources.ImageStream, resources.tag)
-        deployEnvironment(deployNamespace, resources.DeploymentConfig, resources.Service, resources.Route)
+    stage ("Deploy to ${args.env}") {
+        tagImageToDeployEnv(deployNamespace, userNamespace, args.app.ImageStream, args.app.tag)
+        deployEnvironment(deployNamespace, args.app.DeploymentConfig, args.app.Service, args.app.Route)
     }
 }
 
