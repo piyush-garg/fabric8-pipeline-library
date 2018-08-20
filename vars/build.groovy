2e46f2f2468bb5382eca680c60b39be6f97a4dff
hriships <hrishishinde.p@gmail.com>
2018-08-20 15:15:42 +0530

    added build stage

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,5 +1,46 @@
 #!/usr/bin/groovy
 
-def call(resources) {
-    echo "resource ${resources}"
+util
+def call(resources, namespace) {
+    stage("Build application") {
+        def utils = new io.fabric8.Utils()
+        namespace = namespace ?: utils.getUsersNamespace()
+
+        createImageStream(resources.ImageStream, namespace)
+        buildProject(resources.BuildConfig, namespace)
+    }
+
+    post {
+        always {
+            echo 'Hope is the best thing!!!'
+        }
+    }
+}
+
+def createImageStream(imageStream, namespace) {
+    def isName = imageStream.metadata.name
+    def isFound = shWithOutput("oc get is/$isName -n $namespace --ignore-not-found")
+    if (!isFound) {
+        ocApplyResource(imageStream, namespace)
+    } else {
+        echo "image stream exist ${isName}"
+    }
+}
+
+def buildProject(buildConfig, namespace) {
+    ocApplyResource(buildConfig, namespace)
+    openshiftBuild(buildConfig: "${buildConfig.metadata.name}", showBuildLogs: 'true')
+}
+
+def shWithOutput(String command) {
+    return sh(
+            script: command,
+            returnStdout: true
+    ).trim()
+}
+
+def ocApplyResource(resource, namespace) {
+    def resourceFile = "/tmp/${namespace}-${env.BUILD_NUMBER}-${resource.kind}.yaml"
+    writeYaml file: resourceFile, data: resource
+    sh "oc apply -f ${resourceFile} -n ${namespace}"
 }
