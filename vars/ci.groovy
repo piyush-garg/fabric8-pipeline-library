5826fcc887f5a9e954b3a032235326dec9dbf7f7
hriships <hrishishinde.p@gmail.com>
2018-08-20 19:42:30 +0530

    ci

---
+++ vars/ci.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/groovy
+
+def call(Map args = [:], Closure body) {
+    //def resources = processTemplate(templateConfig)
+    //def config = [templateConfig1: "cd"]
+    def targetBranch = args.branch ?: /^PR-\d+$/
+
+    if (env.BRANCH_NAME ==~ targetBranch) {
+        //echo "templateconfig ${body.templateConfig}"
+        //echo "resoures ${resources}"
+        //echo "resources ${body.resources}"
+        //body.resolveStrategy = Closure.DELEGATE_FIRST
+        //body.delegate = config
+        body()
+        //echo "cd config ${config}"
+    }
+}
