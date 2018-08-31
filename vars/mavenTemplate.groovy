63a74c6271ee8a9e998c9cc9906a226c8c73dc8a
Piyush Garg <piyush1594@users.noreply.github.com>
2018-08-31 13:14:19 +0530

    Update mavenTemplate.groovy

--- vars/mavenTemplate.groovy
+++ vars/mavenTemplate.groovy
@@ -9,7 +9,7 @@ def call(Map parameters = [:], body) {
     def defaultLabel = buildId('maven')
     def label = parameters.get('label', defaultLabel)
 
-    def mavenImage = parameters.get('mavenImage', 'fabric8/maven-builder:vd81dedb')
+    def mavenImage = parameters.get('mavenImage', 'openshift/jenkins-slave-maven-centos7')
     def jnlpImage = (flow.isOpenShift()) ? 'fabric8/jenkins-slave-base-centos7:vb0268ae' : 'jenkinsci/jnlp-slave:2.62'
     def inheritFrom = parameters.get('inheritFrom', 'base')
 
