54cda138bb958195ff6faa82597cac769628b779
hriships <hrishishinde.p@gmail.com>
2018-09-03 10:05:04 +0530

    basic eventing

---
+++ src/io/fabric8/plugins/analytics.groovy
@@ -0,0 +1,10 @@
+package io.fabric8.plugins
+
+import io.fabric8.events
+
+def register() {
+    events.on("pipeline.start") {
+        sh "ls ."
+    }
+}
+
--- vars/osio.groovy
+++ vars/osio.groovy
@@ -1,13 +1,24 @@
 #!/usr/bin/groovy
 
+import io.fabric8.events
+import io.fabric8.plugins.*
+
 def call(Map parameters = [:], Closure body) {
 
     def defaultLabel = buildId('nodejs')
     def label = parameters.get('label', defaultLabel)
     def resources = "hello"
+
     //def config = [templateConfig: "nodeJs"]
     //def config = ["template":"processed templateg"]
     node {
+        // loading
+        new analytics().register()
+
+        //TODO: delete this
+
+        events.emit("pipeline.start", "testarg")
+
         checkout scm
 
         //body.resolveStrategy = Closure.DELEGATE_FIRST
