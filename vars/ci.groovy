bdbcf4cf294f8559c06c0cec7bcd95c98450bd68
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2018-09-05 01:19:58 +1000

    ci: minor whitespace correction

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -2,9 +2,8 @@
 
 def call(Map args = [:], Closure body) {
 
-    def targetBranch = args.branch ?: /^PR-\d+$/
-
-    if (env.BRANCH_NAME.trim() ==~ targetBranch) {
-        body()
-    }
+  def targetBranch = args.branch ?: /^PR-\d+$/
+  if (env.BRANCH_NAME.trim() ==~ targetBranch) {
+    body()
+  }
 }
