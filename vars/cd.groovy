dfeadcd4dc6c43ffd0698d65472674af4403081a
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2018-09-05 09:59:04 +1000

    cd: add missing args

--- vars/cd.groovy
+++ vars/cd.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/groovy
 
-def call(body = null) {
-  def targetBranch = args.branch ?: 'master'
-  if (env.BRANCH_NAME.equals(targetBranch)) {
-    body()
-  }
+def call(args = [:], Closure body) {
+    def targetBranch = args.branch ?: 'master'
+    if (env.BRANCH_NAME.equals(targetBranch)) {
+        body()
+    }
 }
