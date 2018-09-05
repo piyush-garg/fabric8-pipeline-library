d644dc4a44681270adc0acfcc0539b27e5d4087e
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2018-09-05 11:56:29 +1000

    pod: refactor

--- vars/pod.groovy
+++ vars/pod.groovy
@@ -3,41 +3,54 @@ import io.fabric8.Fabric8Commands
 
 def call(Map args = [:], body = null) {
     def flow = new Fabric8Commands()
-    def label = buildId('nodejs')
     def cloud = flow.getCloudConfig()
-    def inheritFrom = 'base'
-    def jnlpImage = (flow.isOpenShift()) ? 'fabric8/jenkins-slave-base-centos7:vb0268ae' : 'jenkinsci/jnlp-slave:2.62'
+    def label = buildId(args.name)
 
-    podTemplate(cloud: cloud, label: label, serviceAccount: 'jenkins', inheritFrom: "${inheritFrom}",
-            containers: [
-                    containerTemplate(
-                            name: args.containerName,
-                            image: args.image,
-                            command: '/bin/sh -c',
-                            args: 'cat',
-                            ttyEnabled: true,
-                            workingDir: '/home/jenkins/',
-                            resourceLimitMemory: '640Mi'
-                    ),
-                    containerTemplate(
-                            name: 'jnlp',
-                            image: "${jnlpImage}",
-                            args: '${computer.jnlpmac} ${computer.name}',
-                            workingDir: '/home/jenkins/',
-                            resourceLimitMemory: '256Mi'
-                    )
-            ],
-            volumes: [
-                    secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg-ro'),
-                    secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
-                    secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh-ro'),
-                    secretVolume(secretName: 'jenkins-git-ssh', mountPath: '/root/.ssh-git-ro')
-            ]
+    podTemplate(
+      label: label,
+      cloud: flow.getCloudConfig()
+      serviceAccount: 'jenkins',
+      inheritFrom: 'base',
+      containers: [ slaveTemplate(args.image, ) jnlpTemplate() ],
+      volumes:  volumes()
     ) {
-        node (label) {
-            container(name: args.containerName, shell: args.shell) {
-                body()
-            }
+      node (label) {
+        container(name: args.name, shell: args.shell) {
+            body()
         }
+      }
     }
+
+}
+
+def volumes() {
+  [
+    secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg-ro'),
+    secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
+    secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh-ro'),
+    secretVolume(secretName: 'jenkins-git-ssh', mountPath: '/root/.ssh-git-ro')
+  ]
+}
+
+def slaveTemplate(name, image, shell) {
+    containerTemplate(
+        name: name,
+        image: image,
+        command: "$shell -c",
+        args: 'cat',
+        ttyEnabled: true,
+        workingDir: '/home/jenkins/',
+        resourceLimitMemory: '640Mi'
+    )
+}
+
+def jnlpContainer() {
+    def jnlpImage = 'fabric8/jenkins-slave-base-centos7:vb0268ae'
+    return containerTemplate(
+        name: 'jnlp',
+        image: "${jnlpImage}",
+        args: '${computer.jnlpmac} ${computer.name}',
+        workingDir: '/home/jenkins/',
+        resourceLimitMemory: '256Mi'
+    )
 }
