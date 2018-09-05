3a3086628c8806ad3b11f75677cc4e7fef236d58
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2018-09-05 11:58:32 +1000

    osio: minor whitespace change

--- vars/osio.groovy
+++ vars/osio.groovy
@@ -4,16 +4,13 @@ import io.fabric8.Events
 import io.fabric8.plugins.*
 
 def call(body) {
-
     node {
-
+        // TODO: move registration to a different file; perhaps
+        // plugins.register()?
         new analytics().register()
         Events.emit("pipeline.start", "testarg")
-
         checkout scm
-
         body()
-
         Events.emit("pipeline.end", "testarg")
     }
 }
