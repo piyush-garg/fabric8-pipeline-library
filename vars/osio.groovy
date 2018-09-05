5830aa224407601135079b8fae511e2b7059ff94
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2018-09-05 15:43:06 +1000

    Refactor plugins registration

--- vars/osio.groovy
+++ vars/osio.groovy
@@ -1,13 +1,12 @@
 #!/usr/bin/groovy
 
 import io.fabric8.Events
-import io.fabric8.plugins.*
+import io.fabric8.Plugins
 
 def call(body) {
     node {
         // TODO: move registration to a different file; perhaps
-        // plugins.register()?
-        // new analytics().register()
+        Plugins.register()
         Events.emit("pipeline.start", "testarg")
         checkout scm
         spawn(image: "oc") { body() }
