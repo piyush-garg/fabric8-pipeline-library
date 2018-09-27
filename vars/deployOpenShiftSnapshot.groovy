46a3a3a6bbbbb61ed4b1554f68b7be783512cbbd
Piyush Garg <piyushgarg001@gmail.com>
2018-09-28 02:01:30 +0530

    Fix docs breaking

--- vars/deployOpenShiftSnapshot.groovy
+++ vars/deployOpenShiftSnapshot.groovy
@@ -46,7 +46,7 @@ def call(body) {
         flow.setupK8sConfig()
 
         try {
-            sh "service docker start"
+            sh "sudo systemctl start docker"
             sh "oc cluster up"
             sh "oc get project ${openShiftProject} | grep Active"
         } catch (err) {
